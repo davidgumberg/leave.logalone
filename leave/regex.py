21c26a31e78f6a38c0575784a83c7fccb29c435d
David Gumberg <davidzgumberg@gmail.com>
2026-05-27 15:30:33 -0700

    regex: fmt_to_str add grouped param
    Ungrouped regexes are useful for performance, let's make em.

--- leave/regex.py
+++ leave/regex.py
@@ -1,6 +1,5 @@
 import re
 
-
 def regex_add_names(pattern: str, names: list[str]) -> re.Pattern:
     """
     Takes a regex and adds a list of names for all of the '(.*)' matches
@@ -22,7 +21,7 @@ def regex_add_names(pattern: str, names: list[str]) -> re.Pattern:
     return re.compile(pattern)
 
 
-def fmt_to_regex(fmt_str: str) -> re.Pattern:
+def fmt_to_regex(fmt_str: str, grouped: bool = True) -> re.Pattern:
     """
     Converts a printf/scanf string to a Python Regex.
     Returns the regex string and a list of inferred types, just a wrapper
@@ -41,7 +40,10 @@ def fmt_to_regex(fmt_str: str) -> re.Pattern:
     # the pattern is pure slop, looks right if i squint right
     specifier_pattern = r'%[-+0 #]*[\d\*]*(\.[\d\*]*)?[hljztL]*[diuoxXfFeEgGaAcspn]'
 
-    pattern = re.sub(specifier_pattern, r'(.*)', pattern)
+    if grouped:
+        pattern = re.sub(specifier_pattern, r'(.*)', pattern)
+    else:
+        pattern = re.sub(specifier_pattern, r'.*', pattern)
     pattern = pattern.replace(percent_placeholder, "%")
 
     return re.compile(pattern)
--- test/test_regex.py
+++ test/test_regex.py
@@ -12,42 +12,56 @@ from leave.regex import (
 EXAMPLE_LOG_FMT = "Peer %d sent us a GETBLOCKTXN for block %s, sending a BLOCKTXN with %u txns. (%u bytes)\n"
 EXAMPLE_LOG_MESSAGE = "2026-01-07T15:41:31.606100Z [cmpctblock] Peer 43 sent us a GETBLOCKTXN for block 00000000000000000000793c9c81354b796e2d8e135a6fb583ef6fbc97ff9850, sending a BLOCKTXN with 3083 txns. (1281149 bytes)"
 
+EXAMPLE_ARG_LIST = [
+    "peerid",
+    "blockhash",
+    "txn_count",
+    "txn_size"
+]
+
 
 class TestRegex(unittest.TestCase):
     def test_fmt_to_regex(self):
+        print("Test that matching works.")
         r = fmt_to_regex(EXAMPLE_LOG_FMT)
         self.assertRegex(EXAMPLE_LOG_MESSAGE, r)
 
+        print("Test that grouped=true is the default behavior.")
+        self.assertEqual(r, fmt_to_regex(EXAMPLE_LOG_FMT, grouped=True))
+
+        print("Test that the right number of groups is present in the pattern.")
+        self.assertEqual(r.groups, 4)
+
+        ungrouped_r = fmt_to_regex(EXAMPLE_LOG_FMT, grouped=False)
+
+        print("Test that grouped=true results in 0 groups.")
+        self.assertEqual(ungrouped_r.groups, 0)
+
+        print("Test that grouped=false also matches.")
+        self.assertRegex(EXAMPLE_LOG_MESSAGE, ungrouped_r)
+
     def test_regex_add_names(self):
         r = fmt_to_regex(EXAMPLE_LOG_FMT)
-
-        arg_list = [
-            "peerid",
-            "blockhash",
-            "txn_count",
-            "txn_size"
-        ]
 
         with self.assertRaisesRegex(ValueError, "incorrect length"):
             empty_list = []
             regex_add_names(r.pattern, empty_list)
 
         with self.assertRaisesRegex(ValueError, "incorrect length"):
-            short_list = arg_list[:-1]
+            short_list = EXAMPLE_ARG_LIST[:-1]
             regex_add_names(r.pattern, short_list)
 
         with self.assertRaisesRegex(ValueError, "incorrect length"):
-            long_list = arg_list + ["extra"]
+            long_list = EXAMPLE_ARG_LIST + ["extra"]
             regex_add_names(r.pattern, long_list)
 
         with self.assertRaisesRegex(ValueError, "unique"):
-            duplicate_list = arg_list[:-1] + ["txn_count"]
+            duplicate_list = EXAMPLE_ARG_LIST[:-1] + ["txn_count"]
             regex_add_names(r.pattern, duplicate_list)
 
-        named_pattern = regex_add_names(r.pattern, arg_list)
+        named_pattern = regex_add_names(r.pattern, EXAMPLE_ARG_LIST)
         match = re.search(named_pattern, EXAMPLE_LOG_MESSAGE)
-        print(named_pattern)
-        self.assertIsNotNone(match)
+        self.assertIsNotNone(match.groupdict())
         matchdict = match.groupdict()
         self.assertEqual(matchdict["peerid"], '43')
         self.assertEqual(matchdict["blockhash"], '00000000000000000000793c9c81354b796e2d8e135a6fb583ef6fbc97ff9850')
